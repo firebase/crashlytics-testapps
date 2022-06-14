4dab85643d85ff6400b153d9df4530e55c7534a5
Marc Richards <marcrichards@google.com>
2022-06-14 09:57:26 -0400

    Modify crash buttons.

--- crashlytics-android-app/app/src/main/java/com/example/crashlytics/crashapp/FirstFragment.java
+++ crashlytics-android-app/app/src/main/java/com/example/crashlytics/crashapp/FirstFragment.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 import com.example.crashlytics.crashapp.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
@@ -40,17 +39,6 @@ public class FirstFragment extends Fragment {
       public void onClick(View view) {
         new CrashyClass().throwBackgroundException("Clicked BG throw", 1000L);
       }});
-
-    /*
-    binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        NavHostFragment.findNavController(FirstFragment.this)
-            .navigate(R.id.action_FirstFragment_to_SecondFragment);
-      }
-    });
-
-     */
   }
 
   @Override
